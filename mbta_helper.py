88ae9e2bbb6ce6b9f564c85eeffd0b86aaefaeb3
Yiming Wang <89873516+1178594022@users.noreply.github.com>
2021-11-04 18:15:30 -0400

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -9,14 +9,15 @@ MBTA_API_KEY = ""
 
 # A little bit of scaffolding if you want to use it
 
-import urllib
+import urllib.request
+import json
+
 
 def get_json(url):
     operUrl = urllib.request.urlopen(url)
     data = operUrl.read()
     return data
 
-print(get_json('https://api-v3.mbta.com/stops'))
 
 def get_lat_long(place_name):
     """
@@ -25,7 +26,17 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+
+    APIKEY = 'jQgutkIaKpzETRioBxfWDLFBloBfG5aI'
+    location  = place_name
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={APIKEY}&location={location}'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    Location = (response_data['results'][0]['locations'][0]['displayLatLng']['lat'],response_data['results'][0]['locations'][0]['displayLatLng']['lng'])
+    return Location
+
+print(get_lat_long('Boston,MA'))
 
 
 def get_nearest_station(latitude, longitude):
@@ -35,7 +46,15 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    APIKEY = 'db0316857dfa4ca7883570a48992b29d'
+    location  = place_name
+    url = f'https://www.mbta.com/data/{inlatitudedex}/attributes/latitude, /data/{index}/attributes/longitude'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    Location = (response_data['results'][0]['locations'][0]['displayLatLng']['lat'],response_data['results'][0]['locations'][0]['displayLatLng']['lng'])
+    return Location
+    
 
 
 def find_stop_near(place_name):
